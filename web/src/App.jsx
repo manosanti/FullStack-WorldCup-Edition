219f689416e4f21a43f18d6e38e166dac0b9a055
Lucas Santiago <luccassantiago.contato@gmail.com>
2022-10-05 03:58:12 -0300

    Main

---
+++ web/src/App.jsx
@@ -0,0 +1,15 @@
+export default function App() {
+  return (
+    <div className="h-screen bg-red-700 text-white flex flex-col space-y-6">
+      <h1 className="text-3xl text-center font-bold">Dê o seu palpite na Copa do Mundo do Catar 2022!</h1>
+    
+    <button className="text-red-700 bg-white text-xl px-8 py-4 rounded-xl">
+      Criar minha conta
+    </button>
+
+    <button className="text-white-700 border border-white text-xl px-8 py-4 rounded-xl">
+      Criar minha conta
+    </button>
+    </div>
+  )
+}
